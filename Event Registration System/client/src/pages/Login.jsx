dded142f542a6a4afe6a748424d5725247e97065
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-12-25 22:27:15 +0600

    Remove commented-out data display in Login component

--- Event Registration System/client/src/pages/Login.jsx
+++ Event Registration System/client/src/pages/Login.jsx
@@ -19,7 +19,6 @@ function Login() {
 
   return (
     <div className="max-w-md mx-auto mt-8">
-      {/* <p>Data is {data}</p> */}
       <h2 className="text-2xl font-bold text-center">Login</h2>
       <AuthForm onSubmit={handleSubmit} isLogin={true} />
     </div>
